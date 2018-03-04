143468413bc022ff2a989482d2743aae0db60c25
Alexandr Bezobrazov <alexandr.bezobrazov@gmail.com>
2018-03-04 12:23:42 +0200

    simple blockchain

---
+++ blockchain.js
@@ -0,0 +1,16 @@
+const Block = require('./Block');
+
+class Blockchain {
+    constructor() {
+        this.chain = [Block.genesis( )];
+    }
+
+    addBlock(data) {
+         const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
+        this.chain.push(block);
+
+        return block;
+    }
+}
+
+module.exports = Blockchain;
